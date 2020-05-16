6397d72ab0ea034543d3b8a7b6186b8d6d788df7
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-05-16 11:30:17 -0700

    Include manually generated config.h and project.h

---
+++ generated/mp4v2/project.h
@@ -0,0 +1,31 @@
+#ifndef MP4V2_PROJECT_H
+#define MP4V2_PROJECT_H
+
+/*****************************************************************************/
+
+#define MP4V2_PROJECT_name            "MP4v2"
+#define MP4V2_PROJECT_name_lower      "mp4v2"
+#define MP4V2_PROJECT_name_upper      "MP4V2"
+#define MP4V2_PROJECT_name_formal     "MP4v2 -r"
+#define MP4V2_PROJECT_url_website     "https://github.com/TrevorSundberg/h264_mp4_encoder.git"
+#define MP4V2_PROJECT_url_downloads   "https://github.com/TrevorSundberg/h264_mp4_encoder.git"
+#define MP4V2_PROJECT_url_discussion  "https://github.com/TrevorSundberg/h264_mp4_encoder.git"
+#define MP4V2_PROJECT_irc             ""
+#define MP4V2_PROJECT_bugreport       "<trevor.sundberg@gmail.com>"
+#define MP4V2_PROJECT_version         "-r"
+#define MP4V2_PROJECT_version_hex     0x00020100
+#define MP4V2_PROJECT_version_major   2
+#define MP4V2_PROJECT_version_minor   1
+#define MP4V2_PROJECT_version_point   0
+#define MP4V2_PROJECT_repo_url        "https://github.com/TrevorSundberg/h264_mp4_encoder.git"
+#define MP4V2_PROJECT_repo_branch     "master"
+#define MP4V2_PROJECT_repo_root       "https://github.com/TrevorSundberg/h264_mp4_encoder.git"
+#define MP4V2_PROJECT_repo_uuid       "00000000-0000-0000-0000-000000000000"
+#define MP4V2_PROJECT_repo_rev        0
+#define MP4V2_PROJECT_repo_date       "unknown"
+#define MP4V2_PROJECT_repo_type       "developer"
+#define MP4V2_PROJECT_build           "Wed May 14 00:00:00 PDT 2020"
+
+/*****************************************************************************/
+
+#endif /* MP4V2_PROJECT_H */
